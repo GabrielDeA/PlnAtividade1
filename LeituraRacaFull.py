e1dec1f016505aae433c2b3627f75f0a142fe64c
Gabriel de Souza Klauck <112292289+GabrielSKlauck@users.noreply.github.com>
2025-08-31 21:07:09 -0300

    Inicio da criação de pastas para cada raça

--- LeituraRacaFull.py
+++ LeituraRacaFull.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import os
 
 driver = webdriver.Chrome()
 driver.get("https://dnd5e.wikidot.com/#toc2")
@@ -10,8 +11,14 @@ try:
     ListaRacaComum = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/main/div/div/div/div/div[4]/div[2]/div[4]/div/div[2]/div/div/div[1]/ul/li/ul')
     ItensListaRacaComum = ListaRacaComum.find_elements(By.TAG_NAME, 'li')
 
+    #Separa o texto de cada ul em uma lista de strings
     ListaNomeRacas = []
     for item in ItensListaRacaComum:
+        try:
+            #cria pasta para cada raca da lista de racas
+            os.mkdir(f'c:/RacasPLN/{item.text}')
+        except Exception:
+            print("Pastas ja criadas")
         ListaNomeRacas.append(item.text)
 
     for item in ListaNomeRacas:
@@ -30,6 +37,14 @@ try:
         except Exception as e:
             descricao = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/main/div/div/div/div/div[4]/p[1]/em/strong').text
             print(f"Descricao: {descricao}")
+        
+        #Popula cada arquivo txt de cada raca         
+        arquivo = open(f'c:/RacasPLN/{nomeRaca}/Player Handbook.txt', 'w+')
+        arquivo.writelines(f"Raca: {item.text}")
+        arquivo.writelines(f"Descrição: {descricao}")
+        arquivo.close()
+
+
 except Exception as e:
     print(e)
 finally:
